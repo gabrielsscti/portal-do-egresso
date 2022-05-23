b0109f3452c4931af13eac2b54e784ed2f65f09c
Gabriel S. S. Costa <gabrielssc.ti@gmail.com>
2022-05-23 16:26:10 -0300

    feat: adds curso cursoegresso and cursoegressokey tests

--- spring-backend/src/test/java/ufma/br/springbackend/SpringBackendApplicationTests.java
+++ spring-backend/src/test/java/ufma/br/springbackend/SpringBackendApplicationTests.java
@@ -3,7 +3,7 @@ package ufma.br.springbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes= SpringBackendApplicationTests.class)
 class SpringBackendApplicationTests {
 
     @Test
